a47db51c1f12d5e55ec5c853fe4259e58a72d4a2
Jennifer Zhang <zhangjx@usc.edu>
2025-04-25 05:40:54 +0000

    Initial commit

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,9 +19,16 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
 // Add prototypes for any helper functions here
-
+bool helper(
+    size_t day, // the current day we are filling up 
+    size_t currSlot, // the current slot we are filling up
+    vector<size_t>& shiftNum, // stores the number of shifts for a specific worker
+    const AvailabilityMatrix& avail, // availability matrix
+    const size_t dailyNeed, // daily need 
+    const size_t maxShifts,
+    DailySchedule& sched 
+  );
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +45,84 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    // Get the number of workers from the availability matrix
+    size_t k = avail[0].size();
+
+    // create a vector to count how many shifts are assigned to each worker
+    // each index represents a worker and the value represents the number of shifts
+    vector<size_t> shiftNum(k, 0);
+
+    bool results = helper(0, 0, shiftNum, avail, dailyNeed, maxShifts, sched);
+    return results;
+}
 
 
+bool helper(
+    size_t day, // the current day we are filling up 
+    size_t currSlot, // the current slot we are filling up
+    vector<size_t>& shiftNum, // stores the numbers of shifts per worker
+    const AvailabilityMatrix& avail, // availability matrix
+    const size_t dailyNeed, // daily need 
+    const size_t maxShifts,
+    DailySchedule& sched
+    )
+{
+
+    // Get the number of days from the availability matrix
+    size_t n = avail.size();
+
+    // base case: we have filled all the days in the schedule
+    if(day == n){
+        return true;
+    }
+
+    // once we fill the slots for the current day, we can recursively move on to the next day
+    if (currSlot == dailyNeed){
+        return helper(day +1, 0, shiftNum, avail, dailyNeed, maxShifts, sched);
+    }
+
+    // Get the number of workers from the availability matrix
+    size_t k = avail[0].size();
+
+    // if we are here, we still need to fill up the slots of the day
+    for(Worker_T worker =0; worker < k; worker++){
+      
+      // check if the worker is availabile and has yet to reach max shifts
+      if(avail[day][worker] && shiftNum[worker] < maxShifts){
+        
+        // create a boolean for the next loop
+        bool booked = false;
+        
+        // check if the worker is already scheduled
+        for (size_t j = 0; j < currSlot; j++){
+          if(sched[day][j] == worker){
+            booked = true;
+            break;
+          }
+        }
+
+        if(!booked){
+          // increase the worker's shift count
+          shiftNum[worker]++;
+          
+          // schedule the worker
+          sched[day][currSlot] = worker;
+
+          // recurse to the next slot
+          bool results = helper(day, currSlot+1, shiftNum, avail, dailyNeed, maxShifts, sched);
+          if(results == true){
+            return true;
+          }
+
+          // backtrack if a valid schedule could not be built (ie. the return true was not hit)
+          shiftNum[worker]--;
+
+        }
+      }
+    }
+
+    // if we are here, that means all workers have been tried and a valid schedule could not be built
+    return false;
 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -7,12 +7,22 @@
 #include <set>
 #endif
 
+#include <unordered_set>
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(
+    const std::string& in,
+    const std::string& floating, // floating letters 
+    const std::set<std::string>& dict,
+    std::set<std::string>& answers, // set to store the results
+    std::string curr_combo, // current combination we are making
+    size_t pos, // position in the word that we are currently at
+    const std::unordered_set<std::string>& prefixes
+);
 
 
 // Definition of primary wordle function
@@ -22,7 +32,98 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<std::string> answers; // set to store the answers
+    std::string curr_combo = ""; // string to build new words
+
+    // filter dictionary based on length of word
+
+    //std::set<std::string> filtered_dict;
+    std::unordered_set<std::string> prefixes;
+    for(const auto& word: dict){
+        if(word.length() == in.length()){
+          //filtered_dict.insert(word);
+        
+          for(size_t len = 1; len <= word.length(); ++len){
+            prefixes.insert(word.substr(0,len));
+        }
+      }
+    }
+
+    
+
+    // call helper function
+    wordleHelper(in, floating, dict, answers, curr_combo, 0, prefixes);
+    
+    // return answers set
+    return answers;
 
 }
 
 // Define any helper functions here
+void wordleHelper(
+    const std::string& in,
+    const std::string& floating, // floating letters that can still be placed and will be continuously updated 
+    const std::set<std::string>& dict,
+    std::set<std::string>& answers, // set to store the results
+    std::string curr_combo, // current combination we are making
+    size_t pos, // position in the word that we are currently at
+    const std::unordered_set<std::string>& prefixes
+    )
+{
+
+    // we hit the base case if our position reaches the end of the word
+    if(pos == in.size()){
+        // check if the word is in the dictionary AND that the floating is empty
+        if(dict.find(curr_combo) != dict.end() && floating.empty()){
+            answers.insert(curr_combo);
+        }
+        return;
+    }
+
+    // // checking if the prefixes exist early on
+    // bool prefix_exists = false;
+    // for(const std::string& word : dict){
+    //   if(word.length() == in.length()&& word.substr(0, pos) == curr_combo){
+    //     prefix_exists = true;
+    //     break;
+    //   }
+    // }
+
+    // if(pos > 0 && !prefix_exists){
+    //   return;
+    //   // we do not need to keep checking
+    // }
+
+    if(prefixes.find(curr_combo) == prefixes.end() && pos > 0){
+      return;
+    }
+
+    if(in.size() - pos < floating.size()){
+      return;
+    }
+
+    // if the current character is a dash, we need to fill it
+    if(in[pos] == '-'){
+        
+        for(size_t i = 0; i < floating.size(); i++){
+            char floating_letter = floating[i];
+            std::string updated_floating = floating;
+            updated_floating.erase(i, 1);
+            wordleHelper(in, updated_floating, dict, answers, curr_combo + floating_letter, pos +1, prefixes );
+        }
+        // iterate through a to z
+        for (char letter = 'a'; letter <= 'z'; letter++){
+            size_t pos_floating = floating.find(letter);
+            if(pos_floating == std::string::npos){ 
+                wordleHelper(in, floating, dict, answers, curr_combo+letter, pos +1, prefixes);
+            }
+        }
+        
+    }
+    // if the current character is already fixed, use it and move on
+    else{
+        curr_combo = curr_combo + in[pos];
+        wordleHelper(in, floating, dict, answers, curr_combo, pos+1, prefixes);
+    }
+}
+
